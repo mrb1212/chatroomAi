cf1e1e1d3a7afde2737be588e840a08b0cf22a66
mrb1212 <mohammadrezabahrami1212@gmail.com>
2025-02-03 15:45:38 +0330

    feat: add modal component and update dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV !== 'production';
 
     
-export const server = !dev ? 'http://localhost:3001' : 'https://keytex.ir';
+export const server = dev ? 'http://localhost:3001' : 'https://keytex.ir';
 
 export const test = `${server}/api/AUTHS`
 
